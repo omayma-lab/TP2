d7964f8cdcdadf68317d44ce04b3a954fe29f1d0
omayma <ojaa09411@gmail.com>
2025-12-14 00:49:31 +0100

    Cycle 2 :Test tourner(2) pass avec modification

--- src/main/java/com/omayma/tdd/Personnage.java
+++ src/main/java/com/omayma/tdd/Personnage.java
@@ -2,6 +2,9 @@ package com.omayma.tdd;
 
 public class Personnage {
     public Orientation tourner(int nbr) {
-        return Orientation.EST;
+        if(nbr>1) {
+            return Orientation.SUD;
+        }
+        else return Orientation.EST;
     }
 }
