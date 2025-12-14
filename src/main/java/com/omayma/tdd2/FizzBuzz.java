20dd0a1c865fbfb6eddf6dfabf0571fcf667c52b
omayma <ojaa09411@gmail.com>
2025-12-14 14:02:36 +0100

    Cycle 5 :modification minimale méthode de(nbre)+Execution test

--- src/main/java/com/omayma/tdd2/FizzBuzz.java
+++ src/main/java/com/omayma/tdd2/FizzBuzz.java
@@ -2,6 +2,7 @@ package com.omayma.tdd2;
 
 public class FizzBuzz {
     public static String de(int nbre){
+        if (nbre % 3 == 0 && nbre %5 ==0) return "FizzBuzz";
         if (nbre % 3 == 0) return "Fizz";
         if (nbre % 5 == 0) return "Buzz";
         return String.valueOf(nbre);
