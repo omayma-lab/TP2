c629944fba3ba3b3bb8c152014b6177b866c7e47
omayma <ojaa09411@gmail.com>
2025-12-14 13:51:17 +0100

    Cycle 4 : modification minimal de méthode ad()+Execution test

--- src/main/java/com/omayma/tdd2/FizzBuzz.java
+++ src/main/java/com/omayma/tdd2/FizzBuzz.java
@@ -3,6 +3,7 @@ package com.omayma.tdd2;
 public class FizzBuzz {
     public static String de(int nbre){
         if (nbre % 3 == 0) return "Fizz";
+        if (nbre % 5 == 0) return "Buzz";
         return String.valueOf(nbre);
 
     }
