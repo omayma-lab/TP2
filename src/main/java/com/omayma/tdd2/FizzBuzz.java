6da8071a9ffc754a12f157532bbb44927520d9ce
omayma <ojaa09411@gmail.com>
2025-12-14 14:27:21 +0100

    Refactoring méthode de(nbre)

--- src/main/java/com/omayma/tdd2/FizzBuzz.java
+++ src/main/java/com/omayma/tdd2/FizzBuzz.java
@@ -2,10 +2,11 @@ package com.omayma.tdd2;
 
 public class FizzBuzz {
     public static String de(int nbre){
-        if (nbre % 3 == 0 && nbre %5 ==0) return "FizzBuzz";
+        String res="";
         if (nbre % 3 == 0) return "Fizz";
         if (nbre % 5 == 0) return "Buzz";
-        return String.valueOf(nbre);
+        return res.isEmpty()?
+                String.valueOf(nbre):res;
 
     }
 }
--- src/test/java/com/omayma/tdd2/FizzBuzzTest.java
+++ src/test/java/com/omayma/tdd2/FizzBuzzTest.java
@@ -1,8 +1,11 @@
 package com.omayma.tdd2;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class FizzBuzzTest {
     @Test
@@ -29,6 +32,11 @@ public class FizzBuzzTest {
     void fizzBuzz_de_15_retourn_fizzbuzz(){
 
         assertEquals("FizzBuzz",FizzBuzz.de(15));
+    }
+    @ParameterizedTest
+    @ValueSource(ints ={1,2,3,4,5,6,7,8,9,10,15,20})
+    void fizzbuzz_test_parametre(int n){
+        assertNotNull(FizzBuzz.de(n));
     }
 
 }
